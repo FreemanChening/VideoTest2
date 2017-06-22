92a8bdb302e32c68a8274d4e02e790888607fae9
ch <783015427@qq.com>
2017-06-22 16:50:00 +0800

    第一次提交，作为界面基础

---
+++ app/src/main/java/com/example/admin/videotest3/View1Activity.java
@@ -0,0 +1,13 @@
+package com.example.admin.videotest3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class View1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view1);
+    }
+}
